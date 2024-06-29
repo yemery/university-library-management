6a02060e3c4aaf7172fdbe9caed6d6821aa13ffd
burzum <meryemajmani33@gmail.com>
2024-06-29 14:22:07 +0200

    fixed date changing only in selection

--- frontend/src/components/molecules/EditBorrowDates.jsx
+++ frontend/src/components/molecules/EditBorrowDates.jsx
@@ -3,38 +3,33 @@ import React from "react";
 import H5 from "../atoms/H5";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 // import { set } from "react-datepicker/dist/date_utils";
 const EditBorrowDates = () => {
-    // nested for borrow date and return date
-    const [dates, setDates] = useState({
-        borrowDate: new Date(),
-        returnDate: new Date(),
-    },);
-    useEffect(() => {
+  // nested for borrow date and return date
+  const [dates, setDates] = useState({
+    borrowDate: "",
+    returnDate: "",
+  });
+  useEffect(() => {
     console.log(dates);
+  }, [dates]);
 
-    }, [dates])
-  
-    // added destructuring for borrow date and return date bcs it was not working 
-    const { borrowDate, returnDate } = dates; 
+  // added destructuring for borrow date and return date bcs it was not working
+  const { borrowDate, returnDate } = dates;
 
   return (
     <div className="space-y-7">
       <H5 label="Edit Borrow Dates" />
       <div className="space-y-3">
         <H5 label="Edit Borrow Date" />
-        <DatePicker selected={borrowDate} onChange={(date) => setDates({ ...dates, borrowDate: date })  } />
-
+        <DatePicker selected={borrowDate} onSelect={(date) => setDates({ ...dates, borrowDate: date })  } />
+        {/* <Datepicker inline /> */}
       </div>
       <div className="space-y-3">
         <H5 label="Edit Return Date" />
-        <DatePicker selected={returnDate} onChange={(date) => setDates({ ...dates, returnDate: date })} />
-       
-
+        <DatePicker selected={returnDate} onSelect={(date) => setDates({ ...dates, returnDate: date })  } />
       </div>
-      
-
     </div>
   );
 };
