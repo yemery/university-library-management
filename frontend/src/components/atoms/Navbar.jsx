8e85095b2f2e3049ba65eb7f1a5588e968de4d83
sifeddineEddr <sifeddine.edr@gmail.com>
2024-06-30 04:26:24 +0100

    removed settings from navbar dropdown && fixed navbar z-index

--- frontend/src/components/atoms/Navbar.jsx
+++ frontend/src/components/atoms/Navbar.jsx
@@ -1,17 +1,15 @@
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
-import Logo from "./Logo";
 import {
-  dropDownItems,
   librarianNavLinks,
   studentLinks,
   adminNavLinks,
 } from "../../assets/navigationLinks";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
+
 function Navbars() {
   const role = useSelector((state) => state.auth.role);
   const user = useSelector((state) => state.auth.user);
@@ -32,48 +30,32 @@ function Navbars() {
 
   const navigateToProfile = () => navigate(`/${role}/profile`);
   return (
-    <Navbar fluid rounded className="fixed w-full">
+    <Navbar fluid rounded className="fixed w-full z-10">
       <Navbar.Brand href="/">
-        {/* fix logo component not appearing later !! */}
         <img src="/logo.svg" className="w-12 h-12" alt="Flowbite React Logo" />
-        {/* <Logo/> */}
-        {/* <Logo/> */}
-        {/* <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite React
-        </span> */}
       </Navbar.Brand>
-      {/* drop down with z-index in front */}
-      <div className="flex md:order-2">  
+      <div className="flex md:order-2">
         <Dropdown
           arrowIcon={false}
           className="z-40"
           inline
-          label={
-            <Avatar
-              alt="User settings"
-              img="/images/avatar.jpg"
-              className="bg-black"
-              rounded
-            />
-          }
+          label={<Avatar alt="User settings" img="/menu.svg" />}
         >
           <Dropdown.Header>
-            {/* later with redux we will add curring connected user */}
-            <span className="block text-sm">{user.first_name} {user.last_name}</span>
+            <span className="block text-sm">
+              {user.first_name} {user.last_name}
+            </span>
             <span className="block truncate text-sm font-medium">
               {user.email}
             </span>
           </Dropdown.Header>
           <Dropdown.Item onClick={navigateToProfile}>Profile</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
           <Dropdown.Divider />
           <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
         </Dropdown>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        {/* add later navbar depending on links */}
-        {/* call navlinks of current role */}
         {NavLinks.map((link) => {
           return (
             <NavLink
@@ -89,15 +71,6 @@ function Navbars() {
             </NavLink>
           );
         })}
-
-        {}
-        {/* <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link> */}
       </Navbar.Collapse>
     </Navbar>
   );
